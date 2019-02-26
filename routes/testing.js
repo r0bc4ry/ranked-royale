4089cf093bea07c7a0bc8c532551d20fb722fce6
Rob Cary <r0bc4ry@gmail.com>
2019-02-26 14:59:04 -0500

    Update testing route

--- routes/testing.js
+++ routes/testing.js
@@ -6,16 +6,16 @@ const matchesController = require('../controllers/api/matches-controller');
 const isAuthenticated = require('../helpers/is-authenticated');
 
 router.get('/joinparty', isAuthenticated, async function (req, res, next) {
-    let partyId = req.query.partyId ? req.query.partyId : randomstring.generate({charset: 'hex'});
+    let partyId = req.query.partyId ? req.query.partyId : randomstring.generate({length: 4, charset: 'hex'});
     await matchesController.joinParty(req.user._id.toString(), partyId);
-    res.send('Party joined.');
+    res.send(`Party ${partyId} joined.`);
 });
 
 router.get('/joinmatch', isAuthenticated, async function (req, res, next) {
-    let partyId = req.query.partyId ? req.query.partyId : randomstring.generate({charset: 'hex'});
-    let sessionId = req.query.sessionId ? req.query.sessionId : randomstring.generate({charset: 'hex'});
+    let partyId = req.query.partyId ? req.query.partyId : randomstring.generate({length: 4, charset: 'hex'});
+    let sessionId = req.query.sessionId ? req.query.sessionId : randomstring.generate({length: 4, charset: 'hex'});
     await matchesController.joinMatch(req.user._id.toString(), partyId, sessionId, 100);
-    res.send('Match joined.');
+    res.send(`Party ${partyId} joined match ${sessionId}.`);
 });
 
 module.exports = router;
